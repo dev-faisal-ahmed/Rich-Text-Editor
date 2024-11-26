a4241254943dfdece5a1c552b7f4cc522ae06b79
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-11-26 19:18:20 +0600

    implemented basic markings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { PlateEditor } from "@/components/ui/editor/plate-editor";
-import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function HomePage() {
   return (
-    <main className="p-6 grid grid-rows-[1fr_auto] min-h-screen ">
+    <main className="p-6 grid grid-rows-[1fr_auto] min-h-screen">
       <div />
-      <ScrollArea className="max-h-[250px] border-t">
-        <PlateEditor />
-      </ScrollArea>
+      <PlateEditor />
     </main>
   );
 }
--- src/components/ui/editor/plate-editor.tsx
+++ src/components/ui/editor/plate-editor.tsx
@@ -3,21 +3,26 @@
 import { Plate } from "@udecode/plate-common/react";
 import { useCreateEditor } from "@/components/ui/editor/use-create-editor";
 import { Editor, EditorContainer } from "@/components/ui/plate-ui/editor";
+import { ScrollArea } from "../scroll-area";
+import { Toolbar } from "../plate-ui/toolbar";
 
 interface PlateEditorProps {
   className?: {
     editor?: string;
+    editorContainer?: string;
   };
 }
 
 export function PlateEditor({ className }: PlateEditorProps) {
   const editor = useCreateEditor();
 
-  console.log(editor);
   return (
     <Plate editor={editor}>
-      <EditorContainer>
-        <Editor className={className?.editor} placeholder="Type..." />
+      <EditorContainer className="grid grid-rows-[1fr_auto]">
+        <ScrollArea className="max-h-[50vh]">
+          <Editor className={className?.editor} placeholder="Type..." />
+        </ScrollArea>
+        <Toolbar />
       </EditorContainer>
     </Plate>
   );
